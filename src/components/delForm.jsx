3f3d526368ed3e14330e672222821b7f70c22243
Robel Beyene <robelrb@gmail.com>
2019-01-06 00:29:35 -0500

    Create delForm.jsx

---
+++ src/components/delForm.jsx
@@ -0,0 +1,48 @@
+import React, { Component } from "react";
+import Dialog from "@material-ui/core/Dialog";
+import DialogActions from "@material-ui/core/DialogActions";
+import DialogContent from "@material-ui/core/DialogContent";
+import DialogContentText from "@material-ui/core/DialogContentText";
+import DialogTitle from "@material-ui/core/DialogTitle";
+
+class DelForm extends Component {
+  state = {};
+  render() {
+    if (!this.props.open) {
+      return null;
+    }
+    return (
+      <Dialog
+        open={this.props.open}
+        onClose={this.props.onFormClose(null)}
+        aria-labelledby="alert-dialog-title"
+        aria-describedby="alert-dialog-description"
+      >
+        <DialogTitle id="alert-dialog-title">
+          {"Delete this goal permanently?"}
+        </DialogTitle>
+        <DialogContent>
+          <DialogContentText id="alert-dialog-description">
+            Goal will be deleted (not completed).
+          </DialogContentText>
+        </DialogContent>
+        <DialogActions>
+          <button
+            className="btn btn-success btn-sm"
+            onClick={() => this.props.onFormClose(true)}
+          >
+            Yes
+          </button>
+          <button
+            className="btn btn-danger btn-sm"
+            onClick={() => this.props.onFormClose(false)}
+          >
+            No
+          </button>
+        </DialogActions>
+      </Dialog>
+    );
+  }
+}
+
+export default DelForm;
