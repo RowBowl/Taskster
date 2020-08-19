a0694cafc00fcafddf808b9d7ca08a1ddff7617c
robel <robelrb@gmail.com>
2020-08-19 19:51:58 -0400

    More quick add items

--- src/components/quickAdd.jsx
+++ src/components/quickAdd.jsx
@@ -2,23 +2,31 @@ import React, { Component } from "react";
 
 class QuickAdd extends Component {
 
+    //can be stored elsewhere eventually. Add ability for user to add quick tasks
+    quickAddList = [
+        { id: 1, title: "Budgeting and Finance", body: "Bill payments and Asset Management. Budgeting for the month" },
+        { id: 2, title: "Weight Check", body: "Check weight in the morning" },
+        { id: 3, title: "Clean Room", body: "Clean your room and organize items" },
+        { id: 4, title: "Food Prep", body: "Prepare food for the week." },
+    ]
 
-
-    onDragStart = (ev) => {
+    onDragStart = (ev, item) => {
         //console.log(this.quickTitle," ", this.quickBody);
-        ev.dataTransfer.setData("toAddTitle", "Quick Task 1");
-        ev.dataTransfer.setData("toAddBody", "Quick task 1 body. Due: 2/25");
+        ev.dataTransfer.setData("toAddTitle", item.title);
+        ev.dataTransfer.setData("toAddBody", item.body);
     };
     render() {
 
 
-        return(
-        <div className=" quickCard draggable" draggable
-                onDragStart={(ev)=>this.onDragStart(ev)}>
-            {"Quick Task 1"} 
+        const divItems = this.quickAddList.map((item) =>
+            <div key={item.id} className=" quickCard draggable" draggable
+                onDragStart={(ev) => this.onDragStart(ev,item)}>
+                {item.title}
 
-         </div>
+            </div>
         );
+
+        return ( <div>{divItems}</div>);
     }
 }
 
